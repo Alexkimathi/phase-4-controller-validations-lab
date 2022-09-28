2cf252b71715d9272341718b3be8af722743bf45
alex kimathi <keemalekie@gmail.com>
2022-09-28 16:36:01 +0300

    Done .

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { within: ["Fiction", "Non-Fiction"]}
+
+ 
+    
 end
